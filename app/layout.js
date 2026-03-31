547e36f75e7b7a75f68ea56502e819da78e4d735
illumibot <143239234+illumibot@users.noreply.github.com>
2026-03-31 19:25:36 -0400

    Update layout.js
    Added Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,15 @@ export const metadata = {
 };
 
 import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+
+      {/* Google Analytics */}
+      <GoogleAnalytics gaId="G-KZ8CCT3P5E" />
     </html>
   );
 }
